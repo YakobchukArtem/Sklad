f35840355e2b546c3384e0ea5748ce29c120678a
Artem Yakobchuk <artemyakobchukj@gmail.com>
2023-12-06 17:23:32 +0200

    pdf , excel report

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using Newtonsoft.Json;
 using System.Reflection.Metadata;
 using System.Data.Common;
+using static iText.IO.Image.Jpeg2000ImageData;
 
 
 namespace Sklad.Controllers
@@ -56,203 +57,17 @@ namespace Sklad.Controllers
             DataBase.delete(id);
             return Json(new { success = true });
         }
+        public void Report_PDF()
+        {
+            string filePath = "Report/report.pdf";
+            Report.Print_Report_PDF(DataBase.get(0), filePath);
+        }
+        public IActionResult Report_XLSX()
+        {
+            string filePath = "Report/report.xlsx";
+            Report.Print_Report_XLSX(DataBase.get(0), filePath);
+            return RedirectToAction("Products", DataBase.get(0));
+        }
     }
 
-    public class DataBase
-    {
-        public static List<Product> listProducts = new List<Product>();
-        private static readonly string connectionString;
-        public static bool updateflag = false;
-        public static string current_sort_parameter { get; set; }
-        public static int current_id { get; set; }
-        static DataBase()
-        {
-            try
-            {
-                connectionString = File.ReadAllText("appsettings.secrets.json").Trim();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading configuration: {ex.Message}");
-            }
-        }
-        public static List<Product> get(int id, string parameter = null, string desc=null)
-        {
-            listProducts.Clear();
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    string sortOrder = (desc != null) ? "DESC" : "ASC";
-                    string orderByClause = (!string.IsNullOrEmpty(parameter)) ? $"ORDER BY {parameter} {sortOrder}" : "";
-                    string sql = $"SELECT * FROM Products {orderByClause}";
-                    connection.Open();
-                    if (id > 0)
-                    {
-                        sql = $"SELECT * FROM Products WHERE id = {id}";
-                    }
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Product product = new Product();
-                                product.id = reader.GetInt32(0);
-                                product.name = reader.GetString(1);
-                                product.category = reader.GetString(2);
-                                product.producer = reader.GetString(3);
-                                product.price = reader.GetDecimal(4);
-                                product.count = reader.GetInt32(5);
-                                product.supplier = reader.IsDBNull(6) ? null : reader.GetString(6);
-                                product.measurement_unit = reader.GetString(7);
-                                product.price_unit = reader.GetString(8);
-                                if (!reader.IsDBNull(9))
-                                {
-                                    byte[] imageData = (byte[])reader[9];
-                                    product.image = imageData;
-                                }
-                                listProducts.Add(product);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.ToString());
-            }
-            return listProducts;
-        }
-        public static void add(Product model)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    String sql = "INSERT INTO Products" +
-             "(name, category, producer, price, count, supplier, measurement_unit, price_unit, product_image) VALUES" +
-             "(@name, @category, @producer, @price, @count, @supplier, @measurement_unit, @price_unit, @product_image);";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        command.Parameters.AddWithValue("@name", model.name);
-                        command.Parameters.AddWithValue("@category", model.category);
-                        command.Parameters.AddWithValue("@producer", model.producer);
-                        command.Parameters.AddWithValue("@price", model.price);
-                        command.Parameters.AddWithValue("@count", model.count);
-                        command.Parameters.AddWithValue("@supplier", string.IsNullOrEmpty(model.supplier) ? DBNull.Value : (object)model.supplier);
-                        command.Parameters.AddWithValue("@measurement_unit", model.measurement_unit);
-                        command.Parameters.AddWithValue("@price_unit", model.price_unit);
-                        command.Parameters.Add("@product_image", SqlDbType.VarBinary, -1).Value = model.image != null && model.image.Length > 0 ? model.image : DBNull.Value;
-                        command.ExecuteNonQuery();
-                    }
-
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.ToString());
-            }
-        }
-        public static void delete(int id)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    String sql = "DELETE FROM Products WHERE ID = @Id;";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        command.Parameters.AddWithValue("@Id", id);
-                        command.ExecuteNonQuery();
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.ToString());
-            }
-        }
-        public static void update(int id, Product model)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    String sql = "UPDATE Products SET " +
-                        "name = @name, " +
-                        "category = @category, " +
-                        "producer = @producer, " +
-                        "price = @price, " +
-                        "count = @count, " +
-                        "supplier = @supplier, " +
-                        "measurement_unit = @measurement_unit, " +
-                        "price_unit = @price_unit, " +
-                        "product_image = @product_image " +
-                        "WHERE id = @id;";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        command.Parameters.AddWithValue("@name", model.name);
-                        command.Parameters.AddWithValue("@category", model.category);
-                        command.Parameters.AddWithValue("@producer", model.producer);
-                        command.Parameters.AddWithValue("@price", model.price);
-                        command.Parameters.AddWithValue("@count", model.count);
-                        command.Parameters.AddWithValue("@supplier", string.IsNullOrEmpty(model.supplier) ? DBNull.Value : (object)model.supplier);
-                        command.Parameters.AddWithValue("@measurement_unit", model.measurement_unit);
-                        command.Parameters.AddWithValue("@price_unit", model.price_unit);
-                        command.Parameters.Add("@product_image", SqlDbType.VarBinary, -1).Value = model.image != null && model.image.Length > 0 ? model.image : DBNull.Value;
-                        command.Parameters.AddWithValue("@id", id);
-                        command.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.ToString());
-            }
-        }
-        public static List<String> get(string name)
-        {
-            string query = $"SELECT name FROM {name}";
-            List<String> list = new List<String>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            string categoryName = reader.GetString(0);
-
-                            list.Add(categoryName);
-                        }
-                    }
-                }
-            }
-            return list;
-        }
-        public static void Add(string tablename, string name)
-        {
-            string query = $"INSERT INTO {tablename} VALUES ('{name}')";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-    }
 }
---
+++ Report.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using OfficeOpenXml;
+using Sklad.Models;
+
+namespace Sklad
+{
+    public class Report
+    {
+        static Report()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+        public static void Print_Report_PDF(List<Product> products, string filePath)
+        {
+            using (var writer = new PdfWriter(filePath))
+            {
+                using (var pdf = new PdfDocument(writer))
+                {
+                    var document = new Document(pdf);
+                    document.Add(new Paragraph("Product Report"));
+                    Table table = new Table(8).UseAllAvailableWidth();
+                    table.AddHeaderCell("Name");
+                    table.AddHeaderCell("Category");
+                    table.AddHeaderCell("Producer");
+                    table.AddHeaderCell("Price");
+                    table.AddHeaderCell("Count");
+                    table.AddHeaderCell("Supplier");
+                    table.AddHeaderCell("Measurement Unit");
+                    table.AddHeaderCell("Price Unit");
+                    foreach (var product in products)
+                    {
+                        table.AddCell(product.name);
+                        table.AddCell(product.category);
+                        table.AddCell(product.producer);
+                        table.AddCell(product.price.ToString("C"));
+                        table.AddCell(product.count.ToString());
+                        table.AddCell(product.supplier);
+                        table.AddCell(product.measurement_unit);
+                        table.AddCell(product.price_unit);
+                    }
+                    document.Add(table);
+                }
+            }
+        }
+        public static void Print_Report_XLSX(List<Product> products, string filePath)
+        {
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("ProductReport");
+                worksheet.Cells[1, 1].Value = "Name";
+                worksheet.Cells[1, 2].Value = "Category";
+                worksheet.Cells[1, 3].Value = "Producer";
+                worksheet.Cells[1, 4].Value = "Price";
+                worksheet.Cells[1, 5].Value = "Count";
+                worksheet.Cells[1, 6].Value = "Supplier";
+                worksheet.Cells[1, 7].Value = "Measurement Unit";
+                worksheet.Cells[1, 8].Value = "Price Unit";
+                int row = 2;
+                foreach (var product in products)
+                {
+                    worksheet.Cells[row, 1].Value = product.name;
+                    worksheet.Cells[row, 2].Value = product.category;
+                    worksheet.Cells[row, 3].Value = product.producer;
+                    worksheet.Cells[row, 4].Value = product.price;
+                    worksheet.Cells[row, 5].Value = product.count;
+                    worksheet.Cells[row, 6].Value = product.supplier;
+                    worksheet.Cells[row, 7].Value = product.measurement_unit;
+                    worksheet.Cells[row, 8].Value = product.price_unit;
+                    row++;
+                }
+                package.Save();
+            }
+        }
+    }
+}
