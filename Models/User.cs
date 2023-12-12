5678550851327ed2524d717f2816e6893ed2fcdf
Artem Yakobchuk <artemyakobchukj@gmail.com>
2023-12-12 20:00:08 +0200

    com

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace Sklad.Models
 {
     public class User
     {
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Login is required.")]
         public string Login { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
 
         public bool IsLoggedIn { get; set; }
